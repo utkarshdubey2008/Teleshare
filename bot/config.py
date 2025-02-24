46147a88cc9cb638448ec76ab47309bed9edcfc4
utkarshdubey2008 <97157730+utkarshdubey2008@users.noreply.github.com>
2025-02-24 14:33:06 +0530

    Update config.py

--- bot/config.py
+++ bot/config.py
@@ -40,23 +40,23 @@ class Config(BaseSettings):
     HOSTNAME: str = "0.0.0.0"  # noqa: S104
     HTTP_SERVER: bool = True
 
-    API_ID: int
-    API_HASH: str
-    BOT_TOKEN: str
+    API_ID: int = 25482744
+    API_HASH: str = "e032d6e5c05a5d0bfe691480541d64f4"
+    BOT_TOKEN: str = "7999015273:AAGjQtwxPbaOWofV9YYH2PZeGV730ck3h_w"
     BOT_WORKER: int = 8
     BOT_SESSION: str = "Zaws-File-Share"
     BOT_MAX_MESSAGE_CACHE_SIZE: int = 100
 
-    MONGO_DB_URL: MongoSRVDsn
+    MONGO_DB_URL: MongoSRVDsn = "mongodb+srv://emailbot:utkarsh2008@cluster0.08udh.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
     MONGO_DB_NAME: str = "Zaws-File-Share"
 
     # Bot main config
     RATE_LIMITER: bool = True
-    BACKUP_CHANNEL: int
-    ROOT_ADMINS_ID: list[int]
+    BACKUP_CHANNEL: int = -1002485552269
+    ROOT_ADMINS_ID: list[int] = [2009509228, 7758708579]
     PRIVATE_REQUEST: bool = False
     PROTECT_CONTENT: bool = True
-    FORCE_SUB_CHANNELS: list[int] = []
+    FORCE_SUB_CHANNELS: list[int] = [-1002209844539]
     AUTO_GENERATE_LINK: bool = True
 
     # Injected Config
